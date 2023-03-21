de050d3b1b6047f7eaea0e47894ebcfe4cf9d044
dc19679 <58332500+dc19679@users.noreply.github.com>
2023-03-21 20:13:05 +0000

    Update check_env.py

--- check_env.py
+++ check_env.py
@@ -9,10 +9,6 @@ from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.env_checker import check_env
 from gts_env import GeneticToggleEnv
 
-
-
-
-
 env = GeneticToggleEnv()
 
 check_env(env)
@@ -23,60 +19,10 @@ print("Shape:", env.observation_space.shape)
 print("Action space:", env.action_space)
 
 # The reset method is called at the beginning of an episode
-obs = env.reset()
+# obs = env.reset()
 # Sample a random action
 action = env.action_space.sample()
 print("Sampled action:", action)
 obs, reward, done, info = env.step(int(action))
 
 print(obs.shape, reward, done, info)
-
-
-env = GeneticToggleEnv()
-
-model = PPO(MlpPolicy, env, verbose=0)
-
-def evaluate(model, num_episodes=100, deterministic=True):
-    """
-    Evaluate a RL agent
-    :param model: (BaseRLModel object) the RL Agent
-    :param num_episodes: (int) number of episodes to evaluate it
-    :return: (float) Mean reward for the last num_episodes
-    """
-    # This function will only work for a single Environment
-    env = model.get_env()
-    all_episode_rewards = []
-    for i in range(num_episodes):
-        episode_rewards = []
-        done = False
-        obs = env.reset()
-        while not done:
-            # _states are only useful when using LSTM policies
-            action, _states = model.predict(obs, deterministic=deterministic)
-            # here, action, rewards and dones are arrays
-            # because we are using vectorized env
-            obs, reward, done, info = env.step(action)
-            episode_rewards.append(reward)
-
-        all_episode_rewards.append(sum(episode_rewards))
-
-    mean_episode_reward = np.mean(all_episode_rewards)
-    print("Mean reward:", mean_episode_reward, "Num episodes:", num_episodes)
-
-    return mean_episode_reward
-
-# Use a separate environement for evaluation
-eval_env = gym.make('CartPole-v1')
-
-# Random Agent, before training
-mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=100)
-
-print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
-
-# Train the agent for 10000 steps
-model.learn(total_timesteps=10000)
-
-# Evaluate the trained agent
-mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=100)
-
-print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
